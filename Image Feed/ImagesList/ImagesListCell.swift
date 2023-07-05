c38bd311b92e055bf44fbdef689f9ac9d4f6be0c
AlexandrSevastyanov <sanyaseva@gmail.com>
2023-07-05 10:59:00 +0600

    Создание UITable, кастомная настройка, отображение Передача замоканых картинок

---
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by Alexandr Seva on 04.07.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
