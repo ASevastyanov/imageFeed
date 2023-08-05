2f981d2a969bf4860210319b2de3b7d3e898817c
AlexandrSevastyanov <sanyaseva@gmail.com>
2023-08-05 15:59:11 +0600

    Update File

--- Image Feed/Services/Constants.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-    let accessKey = "wagLspRbFqO9RevhMLgjQ_SQ86FDCYV6yet9UPwQLzw"
-    let secretKey = "245FqipR1u_VV3RSkIWg5VZM3VdmBbeog-rjzZrXKoI"
-    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    let accessScope = "public+read_user+write_likes"
-    let defaultBaseURL = URL(string: "https://api.unsplash.com")!
-    let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
